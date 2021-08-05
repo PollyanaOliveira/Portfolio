cd4b4b5647498a53d071ee5e2def5b9344c3c561
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-08-05 15:05:11 -0400

    feat: :sparkles: create Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <!--===== HEADER =====--> */}
+      <header className="l-header">
+        <nav className="nav bd-grid">
+          <div>
+            <a href="#home" className="nav__logo">Pollyana Oliveira</a>
+          </div>
+          <div className="nav__menu">
+            <ul className="nav__list">
+              <li class="nav__item"><a href="#home" class="nav__link active">Home</a></li>
+              <li class="nav__item"><a href="#about" class="nav__link">About</a></li>
+              <li class="nav__item"><a href="#skills" class="nav__link">Skills</a></li>
+              <li class="nav__item"><a href="#work" class="nav__link">Projects</a></li>
+              <li class="nav__item"><a href="#contact" class="nav__link">Contact</a></li>
+            </ul>
+          </div>
+        </nav>
+    </header>
     </div>
   );
 }
