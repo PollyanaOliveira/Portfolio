f64b929c3b8d6528fe2da6a01a0b2b98c52f4c86
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-08-06 20:24:27 -0400

    fix: :art: update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,49 +11,41 @@ import todolist from '../images/portfolio-images/todolist.png';
 
 export default function Projects() {
   return(
-    <section className="portfolio_section">
+    <section className="portfolio section">
       <h2 className="section-title">Projects</h2>
-      <div className="portfolio_container">
+      <div class="portfolio__container bd-grid">
         <div className="portfolio_link">
 
-          <div className="image_portfolio">
-            <img src={panda} alt="" />
-            <a href="https://github.com/PollyanaOliveira/panda-store-project" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+          <a href="https://github.com/PollyanaOliveira/panda-store-project" className="portfolio_details"><img src={panda} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={movie} alt="" />
-            <a href="https://github.com/PollyanaOliveira/project-movie-card-library-crud" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/project-movie-card-library-crud" className="portfolio_details"><img src={movie} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={model} alt="" />
-            <a href="https://github.com/PollyanaOliveira/extras-projects/tree/main/2.Proj_Extras/Anna_Bella" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/extras-projects/tree/main/2.Proj_Extras/Anna_Bella" className="portfolio_details"><img src={model} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={jm} alt="" />
-            <a href="https://github.com/PollyanaOliveira/extras-projects/tree/main/2.Proj_Extras/JM_Viagens" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/extras-projects/tree/main/2.Proj_Extras/JM_Viagens" className="portfolio_details"><img src={jm} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={blog} alt="" />
-            <a href="https://github.com/PollyanaOliveira/extras-projects/tree/main/2.Proj_Extras/TecBlog" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/extras-projects/tree/main/2.Proj_Extras/TecBlog" className="portfolio_details"><img src={blog} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={trivia} alt="" />
-            <a href="https://github.com/PollyanaOliveira/trivia-game" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/trivia-game" className="portfolio_details"><img src={trivia} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={pixels} alt="" />
-            <a href="https://github.com/PollyanaOliveira/pixels-art" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/pixels-art" className="portfolio_details"><img src={pixels} alt="" /></a>
           </div>
 
-          <div className="image_portfolio">
-            <img src={todolist} alt="" />
-            <a href="https://github.com/PollyanaOliveira/project-todo-list" className="portfolio_details">View Details</a>
+          <div className="portfolio__img">
+            <a href="https://github.com/PollyanaOliveira/project-todo-list" className="portfolio_details"><img src={todolist} alt="" /></a>
           </div>
 
         </div>
