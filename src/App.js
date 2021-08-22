9c69195290ef274bf94bf4ffcc78eccd598fd743
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-08-21 22:18:29 -0400

    feat: :zap: create new class
    create new class, create state, create menu hamburguer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+import { useEffect, useState } from 'react';
+
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import {FaWhatsapp} from 'react-icons/fa';
 import {FaLinkedinIn, FaGithub} from 'react-icons/fa';
 import {CgCopyright} from 'react-icons/cg';
-import {GiHamburgerMenu} from 'react-icons/gi';
 
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Animation from './components/Animation';
+import About from './components/About';
 
 import Profile from './images/profile-images/profile.jpeg';
-import { useEffect } from 'react';
 
 export default function App() {
+  const [showMenu, setShowMenu] = useState(false);
   // ---------------------------------------------------------------------------------------------
     // CICLOS DE VIDA
     useEffect(() => { Aos.init({ duration: 2000 }); }, []);
@@ -27,18 +29,21 @@ export default function App() {
           <div>
             <a href="#home" className="nav__logo">Pollyana Oliveira</a>
           </div>
-          <div className="nav__menu" id="nav-menu">
-            <ul className="nav__list">
-              <li className="nav__item"><a href="#home" className="nav__link active">Home</a></li>
+          <div className={ (showMenu) ? 'nav__menu show' : 'nav__menu' } id="nav-menu">
+            <ul className="nav__list" onClick={ () => setShowMenu(!showMenu) }>
+              <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
               <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
               <li className="nav__item"><a href="#skills" className="nav__link">Skills</a></li>
               <li className="nav__item"><a href="#portfolio" className="nav__link">Projects</a></li>
               <li className="nav__item"><a href="#contact" className="nav__link">Contact</a></li>
             </ul>
-            <div className="nav__toggle" id="nav-toggle">
-              <GiHamburgerMenu />
-            </div>
           </div>
+          <button
+              type="button"
+              onClick={ () => setShowMenu(!showMenu) }
+              className={ (showMenu) ? 'icon icon__active' : 'icon' }>
+              <div className="hamburguer" />
+          </button>
         </nav>
     </header>
     <main className="l-main">
@@ -46,7 +51,7 @@ export default function App() {
       <section className="home bd-grid" id="home">
         <div data-aos="fade-down" className="home__data">
           <h1 className="home__title">
-            Hi,<br />I'am<span className="home__title-color"> Pollyana</span><br />Developer
+            Hi,<br />I am<span className="home__title-color"> Pollyana</span><br />Developer
           </h1>
           <a href="#contact" className="button">Contact</a>
         </div>
@@ -64,14 +69,23 @@ export default function App() {
       <section className="about section" id="about">
         <h2 data-aos="fade-down" className="section-title">About</h2>
         <div data-aos="fade-down" className="about__container bd-grid">
-          <div className="about__img">
+          <div data-aos="fade-down" data-aos-delay="700" className="about__img">
             <img src={Profile} alt="profile" />
           </div>
           <div>
-            <h2 className="about__subtitle">I'am Pollyana</h2>
-            <p className="about__text">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate cum expedita quo culpa tempora, assumenda, quis fugiat ut voluptates soluta, aut earum nemo recusandae cumque perferendis! Recusandae alias accusamus atque.
+            <h2 data-aos="fade-down" className="about__subtitle">I'm Pollyana</h2>
+            <p data-aos="fade-down" className="about__text">
+              👩🏽‍💻 I'm a web development studenty at <a href="https://www.betrybe.com/" className="school__link">Trybe</a> School. <br />
+              🤓 I'm currently learning Back-End features. I'm very interested in improving my knowledge in web development. <br />
+              👩🏽‍🎓 I have degree in Education and Accounting. <br />
+              🥦 I'm veggie.<br />
+              🏃🏾‍♀️ I'm constantly on the move. Never accommodate!<br />
+              📍From Campo Grande - MS 🇧🇷 <br />
+              📄 See my <a href = "https://gitconnected.com/pollyanaoliveira/resume" className="curriculum__link">Curriculum Vitae</a> to get more info.
             </p>
+          </div>
+          <div data-aos="fade-down" data-aos-delay="900">
+            <About />
           </div>
         </div>
       </section>
@@ -121,7 +135,7 @@ export default function App() {
                 <a href="https://www.github.com/PollyanaOliveira"><FaGithub className="about_social_icon" /></a>
             </div>
         </div>
-        <p className="footer__copy"><CgCopyright /> 2021 Pollyana Oliveira. All right reserved</p>
+        <p className="footer__copy"><CgCopyright /> 2021 Made 🤍 by Pollyana Oliveira.</p>
       </footer>
     </main>
     </div>
